757f22683915a8e26a6b738f974fdf9a4bf52bc7
yudonggeun <ydong98@gmail.com>
2024-01-20 20:12:59 +0900

    first commit

---
+++ src/main/java/com/practice/testcontainer/resilience_practice/ResiliencePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.testcontainer.resilience_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiliencePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResiliencePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/testcontainer/resilience_practice/ResiliencePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.testcontainer.resilience_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiliencePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
