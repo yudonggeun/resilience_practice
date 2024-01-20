f5873fba959ce725e58fbe4b19ac4f180732dbf4
yudonggeun <ydong98@gmail.com>
2024-01-20 21:27:54 +0900

    wire mock with test container

---
+++ src/test/java/com/practice/testcontainer/resilience_practice/restclient/WireMockRestClientTest.java
@@ -0,0 +1,56 @@
+package com.practice.testcontainer.resilience_practice.restclient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.assertj.core.api.BDDAssertions.then;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestClient;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public class WireMockRestClientTest {
+
+    @RegisterExtension
+    static WireMockExtension wireMock = WireMockExtension.newInstance()
+        .options(
+            wireMockConfig()
+                .dynamicPort()
+        )
+        .build();
+
+    @DisplayName("mocking api server")
+    @Test
+    void test() {
+        // given
+        RestClient restClient = RestClient.builder()
+            .baseUrl("http://localhost:" + wireMock.getPort() + "/")
+            .build();
+
+        String mockResponseBody = """
+            {
+                "mock": "hello world"
+            }
+            """;
+
+        wireMock.stubFor(WireMock.get("/")
+            .willReturn(aResponse()
+                .withBody(mockResponseBody))
+        );
+
+        // when
+        String body = restClient.get()
+            .retrieve()
+            .body(String.class);
+
+        // then
+        System.out.println("response = " + body);
+        then(body).isEqualTo(mockResponseBody);
+    }
+}
